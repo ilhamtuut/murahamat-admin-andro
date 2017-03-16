a2e4fdc55bdb17d6f6894ada43e02995fd072e76
ilhamtuut <ilham.ahmad14@gmail.com>
2017-03-16 23:05:27 +0700

    initial commit

---
+++ app/src/main/java/wad/wan/murahamatdistro/KategoriActivity.java
@@ -0,0 +1,13 @@
+package wad.wan.murahamatdistro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KategoriActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kategori);
+    }
+}
